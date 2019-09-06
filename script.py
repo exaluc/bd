58b5dea1f95fe4c245ac5579eb1e757cbfe1bbe5
Lucian <42606+odgon@users.noreply.github.com>
2019-09-06 19:11:02 +0200

    recherche & achat

--- script.py
+++ script.py
@@ -6,7 +6,10 @@ from selenium import webdriver
 # import sleep pour tineout
 from time import sleep
 
-url = "http://blazedemo.com/index.php"
+# import By
+from selenium.webdriver.common.by import By
+
+url = "http://blazedemo.com/"
 
 # Initie une instance ChromeDriver
 driver = webdriver.Chrome()
@@ -16,3 +19,42 @@ driver.get(url)
 
 # Maximise la fenêtre
 driver.maximize_window()
+
+sleep(2)
+
+# Choix pays départ
+driver.find_element(By.XPATH, "//select[@name='fromPort']/option[text()='Boston']").click()
+
+# Chois pays déstination
+driver.find_element(By.XPATH, "//select[@name='toPort']/option[text()='London']").click()
+
+# Cherche les vols
+driver.find_element(By.CLASS_NAME, 'btn-primary').click()
+
+sleep(2)
+
+# Prends le premier vol dans la liste
+driver.find_element(By.CLASS_NAME, "btn-small").click()
+
+sleep(2)
+
+# Formulaire à remplire
+driver.find_element(By.ID, 'inputName').send_keys("John Doe")
+driver.find_element(By.ID, 'address').send_keys("Rue de Lutèce")
+driver.find_element(By.ID, 'city').send_keys("Paris")
+driver.find_element(By.ID, 'state').send_keys("Île-de-France")
+driver.find_element(By.ID, 'zipCode').send_keys("75001")
+driver.find_element(By.XPATH, "//select[@name='cardType']/option[text()='Visa']").click()
+driver.find_element(By.ID, 'creditCardNumber').send_keys("0000")
+driver.find_element(By.ID, 'nameOnCard').send_keys("John Doe")
+driver.find_element(By.ID, 'creditCardMonth').send_keys("12")
+driver.find_element(By.ID, 'creditCardYear').send_keys("2025")
+
+sleep(5)
+
+# Achat du billet
+driver.find_element(By.CLASS_NAME, 'btn-primary').click()
+
+sleep(15)
+
+driver.quit()
